dbf0d5a28c1c6019caf625c41823c173695a7adc
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-05-15 22:13:24 +0300

    Added exercise 3.1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 import React from "react";
 // import { Exercise2Part1 } from "./components/Exercise2.1";
-import {
-  Exercise2Section1,
-  Exercise2Section2,
-  Exercise2Section3,
-} from "./components/Exercise2.2";
+// import {
+//   Exercise2Section1,
+//   Exercise2Section2,
+//   Exercise2Section3,
+// } from "./components/Exercise2.2";
+import { Exercise3 } from "./components/Ex3.1/Exercise3.1";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         {/* <Exercise2Part1 /> */}
-        <Exercise2Section1 />
+        {/* <Exercise2Section1 />
         <Exercise2Section2 />
-        <Exercise2Section3 />
+        <Exercise2Section3 /> */}
+          <Exercise3></Exercise3>
       </div>
     );
   }
