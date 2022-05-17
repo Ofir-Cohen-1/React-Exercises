3eeab77b8d12ca3651fa716a1683175eb880b244
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-05-18 01:06:06 +0300

    Added exercise 8.3

--- react-life_cycle_methods/src/App.js
+++ react-life_cycle_methods/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 // import Exercise8Part1 from "./components/Exercise8.1/exercise8.1";
-import Exercise8Part2 from "./components/Exercise8.2/exercise8.2";
+// import Exercise8Part2 from "./components/Exercise8.2/exercise8.2";
+import Exercise8Part3 from "./components/Exercise8.3/exercise8.3";
 
 function App() {
   return (
     <div className="App">
       {/* <Exercise8Part1></Exercise8Part1> */}
-      <Exercise8Part2 className="mediumBox"/>
+      {/* <Exercise8Part2 className="mediumBox"/>
       <Exercise8Part2 className="largeBox"/>
-      <Exercise8Part2 className="smallBox"/>
+      <Exercise8Part2 className="smallBox"/> */}
+           <Exercise8Part3></Exercise8Part3>
     </div>
   );
 }
---
+++ react-life_cycle_methods/src/components/Exercise8.3/exercise8.3.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style3.css";
+
+export default class Ex8Part3 extends React.Component {
+  colors = ["red", "blue", "purple", "green", "orange"];
+  state = { colorChangeCounts: 0 };
+
+  componentDidMount() {
+    this.setState({});
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.setState((prevState) => {
+        // console.log(prevState.colorChangeCounts);
+        return {
+          colorChangeCounts: prevState.colorChangeCounts + 1,
+        };
+      });
+    }, 500);
+  }
+
+  squareToCircle = () => {
+    if (this.state.colorChangeCounts > 5) {
+      this.setState((prevState) => {
+        return { radius: "50%" };
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div
+        className="colorfulBox"
+        style={{
+          background: `${this.colors[this.state.colorChangeCounts % 5]}`,
+          borderRadius: this.state.colorChangeCounts >= 5 ? "50%" : "0",
+        }}
+      ></div>
+    );
+  }
+}
