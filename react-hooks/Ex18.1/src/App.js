3463debafac4bcd9cfc97917be5b2560e3cc3088
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-05-29 22:42:41 +0300

    Added exercise 18.2

---
+++ react-hooks/Ex18.1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Text from "./components/Text";
+
+const text = `Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip`;
+
+function App() {
+  return (
+    <div>
+      <Text text={text} textLength={100} />
+    </div>
+  );
+}
+
+export default App;
